4ebcbdf595804b925238fd6894ceb51790adea5c
Your Name <you@example.com>
2023-05-21 20:54:01 +0900

    Add title and description to the swagger client.

---
+++ src/main/java/jp/co/axa/apidemo/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package jp.co.axa.apidemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "AXA Java Challenge",
+                "A simple API to manage employees.",
+                "1.0.0",
+                "",
+                new Contact("Masayoshi Sugimoto", "", "sugimoto.massayoshi@gmail.com"),
+                "",
+                "",
+                Collections.emptyList()
+        );
+    }
+}
